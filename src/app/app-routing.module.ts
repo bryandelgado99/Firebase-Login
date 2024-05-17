ebef2be4260cff20ecd748fbf0ad7e6c8a4fbb63
Bryan Delgado <bryand9970@gmail.com>
2024-05-17 16:42:12 -0500

    Nueva app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CvHomeComponent } from './cv-home/cv-home.component';
+import { CvExpComponent } from './cv-exp/cv-exp.component';
 
 const routes: Routes = [
   {
@@ -10,13 +15,20 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
+  }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  imports:[
+    RouterModule.forRoot([
+      { path: '', component: AppComponent},
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'cv_home', component: CvHomeComponent },
+      { path: 'cv_exp', component: CvExpComponent},
+    ]),
   ],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { RouteReuseStrategy, RouterModule } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,9 +9,13 @@ import { environment } from 'src/environments/environment';
 import {provideAuth, getAuth} from '@angular/fire/auth'
 import {provideFirestore, getFirestore} from '@angular/fire/firestore';
 import {provideStorage, getStorage} from '@angular/fire/storage';
+import { CvHomeComponent } from './cv-home/cv-home.component';
+import { RegisterComponent } from './register/register.component';
+import { CvExpComponent } from './cv-exp/cv-exp.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CvHomeComponent, RegisterComponent, CvExpComponent, LoginComponent],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
@@ -23,5 +27,6 @@ import {provideStorage, getStorage} from '@angular/fire/storage';
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule {}
